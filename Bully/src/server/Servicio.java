4844676ce5c2ecf77dbc06ce7db74ffa53d760d4
Alvaro Torijano <alvarotorijano@gmail.com>
2019-04-04 04:43:49 +0200

    Modificada la funcion "para" a falta de pruebas, pero esperando que funcione

--- Bully/src/server/Servicio.java
+++ Bully/src/server/Servicio.java
@@ -1,13 +1,18 @@
 package server;
 
-import java.util.ArrayList;
+import java.net.URI;
+import java.util.*;
 
 import javax.inject.Singleton;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.UriBuilder;
 
 import clients.Proceso;
 
@@ -20,20 +25,46 @@ public class Servicio {
 	//numero de procesos totales 
 	private static int NUM_PROCESOS = 6;
 	private static ArrayList<Proceso> procesos = new ArrayList<Proceso>();
-
+	private static ArrayList<Integer> idsLibres = new ArrayList<Integer>();
+	private static HashMap<Integer, String> ubicaciones = new HashMap<>();
+	//Este array nos servirá para saber que ids tienen que tener los procesos que nosotros crearemos, tambien quedará alineado con los procesos locales de la maquina, asi cuando tengamos nuestro array de procesos locales, asi no tendremos que encuestar a cada uno de los metodos hasta dar con el que tiene el id que queremos. Otra opcion es recorrer el array haciendo un proceso[i].getID
+	
 	//Metodo main para la creacion y arranque de los procesos, se pasan por argumentos los ids de los procesos
 	public static void main(String[] args) {
 		
-		if(args.length > 1) {
-			for(int i = 1; i< args.length; i++ ) {
-				procesos.add(new Proceso(Integer.parseInt(args[i])));
-				procesos.get(i).start();
-			}
+		for (int i=0; i< NUM_PROCESOS; i++) {
+			idsLibres.add(i+1);
 		}
 		
+		if(args.length > 1) {
+			for(int i = 0; i< args.length;) {
+				/*
+				 * 6	192.168.1.2
+				 * 5	127.0.0.1
+				 * 3	192.168.1.3
+				 * 4	127.0.0.1
+				 * */
+				idsLibres.remove(args[i]);
+				ubicaciones.put(Integer.valueOf(args[i++]), args[i++]);
+				
+				// procesos.add(new Proceso(Integer.parseInt(args[i])));
+				// procesos.get(i).start();
+			}
+		}
+		//ahora que sabemos cuantos procesos remotos hay, generaremos nosotros el numero de ellos que falten.
+		//Para eso hay que comprobar que ids tienen los demas y generar dos que no coincidan
+		for (int i = 0; i< idsLibres.size(); i++) {
+			procesos.add(new Proceso(idsLibres.get(i)));
+		}
+		for(Proceso p : procesos) {
+			p.start();
+		}
+		//Ahora tenemos todos nuestros procesos funcionando
+		
+		/*Se me ocurre que para cada mensaje que un proceso envie, se la envie siempre
+		 * al servidor local, y este a su vez decida si se la tiene que enviar a 
+		 * otra maquina, o ejecutar un metodo de alguno de los procesos que tiene*/
 	}
-	
-	
 	
 	@GET
 	@Produces(MediaType.TEXT_PLAIN)
@@ -44,7 +75,7 @@ public class Servicio {
 			//mirar donde esta cada proceso y enviar mensaje eleccion
 		}
 		
-		return "ElecciÃ³n enviada";
+		return "Eleccion enviada";
 		
 	}
 	
@@ -69,7 +100,7 @@ public class Servicio {
 			if (procesos.get(i).ID == identificador) {
 				procesos.get(i).arrancar();
 			}
-			}
+		}
 		
 		return ("Proceso arrancado");
 	}
@@ -78,12 +109,23 @@ public class Servicio {
 	@Produces(MediaType.TEXT_PLAIN)
 	@Path("para")
 	public String para(@QueryParam(value = "identificador") int identificador) {
+
+		URI uri;
+		WebTarget target;
+		Client cliente = ClientBuilder.newClient();
 		
-		for(int i = 0; i< procesos.size(); i++) {
-			if (procesos.get(i).ID == identificador) {
-				procesos.get(i).parar();;
+		if (ubicaciones.get(identificador) == null) {
+			for(int i=0; i<procesos.size(); i++) {
+				if (procesos.get(i).ID == identificador) {
+					procesos.get(i).parar();
+				}
 			}
-			}
+		} 
+		else {
+			uri = UriBuilder.fromUri("http://" + ubicaciones.get(identificador) + ":8080/Bully").build();
+			target = cliente.target(uri);
+			System.out.println(target.path("rest").path("servicio").path("para").request(MediaType.TEXT_PLAIN).get(String.class));
+		}
 		
 		return ("Proceso parado");
 	}
