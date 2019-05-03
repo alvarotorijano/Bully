45e763da88999257cc628d947fc270dbf06eccd7
Alvaro Torijano <alvarotorijano@usal.es>
2019-05-03 07:42:49 +0200

    Test con scripts 2

--- Bully/deploy/Lauch.sh
+++ Bully/deploy/Lauch.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 
-ssh root@192.168.1.27 "
-sudo apt install default-jre git -y;
-cd ;
-rm -rf apacheServer;
-rm -rf Bully;
-git clone https://github.com/alvarotorijano/Bully.git;
-wget http://ftp.cixug.es/apache/tomcat/tomcat-7/v7.0.94/bin/apache-tomcat-7.0.94.tar.gz;
-tar xvzf apache-tomcat-7.0.94.tar.gz;
-mkdir apacheServer;
-mv apache-tomcat-7.0.94/* apacheServer;
 
-rm apache-tomcat-7.0.94.tar.gz;
-rm -rf apache-tomcat-7.0.94;
+sudo apt install default-jre git -y
+cd 
+rm -rf apacheServer
+# rm -rf Bully
+# git clone https://github.com/alvarotorijano/Bully.git
+wget http://ftp.cixug.es/apache/tomcat/tomcat-7/v7.0.94/bin/apache-tomcat-7.0.94.tar.gz
+tar xvzf apache-tomcat-7.0.94.tar.gz
+mkdir apacheServer
+mv apache-tomcat-7.0.94/* apacheServer
 
-export JAVA_HOME=$(readlink -f /usr/bin/java | sed "s:bin/java::");
-export CATALINA_HOME=~/apacheServer/bin;
+rm apache-tomcat-7.0.94.tar.gz
+rm -rf apache-tomcat-7.0.94
+
+export JAVA_HOME=$(readlink -f /usr/bin/java | sed "s:bin/java::")
+export CATALINA_HOME=~/apacheServer/bin
 
 cp Bully/Bully/deploy/Bully.war apacheServer/webapps/
 
-"
--- Bully/deploy/downloadAndLaunch.sh
+++ Bully/deploy/downloadAndLaunch.sh
@@ -1,4 +1,7 @@
 ssh root@192.168.1.27 "
 rm -rf Bully;
-git clone https://github.com/alvarotorijano/Bully.git
+git clone https://github.com/alvarotorijano/Bully.git;
+cd Bully;
+git checkout deploy;
+./Bully/Bully/deploy/Launch.sh
 "
