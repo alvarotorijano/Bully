634b97a2e2585402222baa60c3bb3013dcdf2c5f
alvaro torijano <alvarotorijano@usal.es>
2019-05-15 02:20:04 +0200

    Modificado el script de lanzamiento y añadido el script que lanza el proyecto

---
+++ Bully/deploy/launchBully.sh
@@ -0,0 +1,9 @@
+
+#linea de argumentos de prueba: 1 192.168.1.1 2 192.168.1.1 3 192.168.1.2
+
+direcciones="$(grep -oE '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' <<< "$@")"  
+maquinas=$(echo "$direcciones"|tr " " "\n"|sort -u|tr "\n" " ")
+echo $maquinas
+
+./downloadAndLaunch.sh $maquinas;
+java -jar $@
